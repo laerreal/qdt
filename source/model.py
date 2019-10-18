040422ebc34c89d0471586afd83b3bb01647adb5
Efimov Vasily <real@ispras.ru>
2019-10-18 21:32:23 +0300

    Structure: remove unused `fields_indent`
    Field indentation is already accounted in `indent` argument.
    
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- source/model.py
+++ source/model.py
@@ -1131,12 +1131,12 @@ class Structure(Type):
 
         if self.declaration is None:
             struct_begin = StructureTypedefDeclarationBegin(self, indent)
-            struct_end = StructureTypedefDeclarationEnd(self, fields_indent,
+            struct_end = StructureTypedefDeclarationEnd(self,
                 indent, True
             )
         else:
             struct_begin = StructureDeclarationBegin(self, indent)
-            struct_end = StructureDeclarationEnd(self, fields_indent,
+            struct_end = StructureDeclarationEnd(self,
                 indent, True
             )
 
@@ -2370,7 +2370,6 @@ class StructureTypedefDeclarationEnd(SourceChunk):
     weight = 2
 
     def __init__(self, struct,
-        fields_indent = "    ",
         indent = "",
         append_nl = True
     ):
@@ -2404,7 +2403,6 @@ class StructureDeclarationEnd(SourceChunk):
     weight = 2
 
     def __init__(self, struct,
-        fields_indent = "    ",
         indent = "",
         append_nl = True
     ):
