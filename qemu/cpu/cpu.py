d51b9e881d8236c79ada8443f69b08638e4da2fd
Efimov Vasily <real@ispras.ru>
2025-03-24 23:02:06 +0300

    CPUDescription: gen_order = -1: generate first
    Signed-off-by: Efimov Vasily <real@ispras.ru>

--- qemu/cpu/cpu.py
+++ qemu/cpu/cpu.py
@@ -1337,6 +1337,8 @@ class CPUType(QOMCPU):
 @descriptionOf(CPUType)
 class CPUDescription(QOMDescription):
 
+    gen_order = -1
+
     def co_gen(self, *a, **kw):
         yield self.gen_type().co_gen(*a, **kw)
 
